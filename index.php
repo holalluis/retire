648c389bdf56b115e822c65d9865781d9419e893
lbosch <lbosch@icra.cat>
2017-05-12 16:15:56 +0100

    inicial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html><html><head>
+	<meta charset=utf-8>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel=stylesheet href=css.css>
+</head><body><center>
+
+<h1>Orwell 3000</h1>
+
+<input placeholder="DNI del ciutadà que vols eliminar">
+
+<form method=GET action="retire.php">
+	<img width=200px src=img/shield.png>
+	<button id=btn>RETIRE</button>
+</form>
+
